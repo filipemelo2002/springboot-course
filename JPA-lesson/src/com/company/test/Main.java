652188a8a8cd0effdd4dd611d437fc09e237e067
Filipe Melo <filipe.melo@liferay.com>
2021-12-04 14:23:52 -0300

    Lesson 1: JPA

---
+++ JPA-lesson/src/com/company/test/Main.java
@@ -0,0 +1,13 @@
+package com.company.test;
+
+import com.company.test.domain.Person;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person p1 = new Person(1, "Filipe", "filipe@gmail.com");
+
+        System.out.println(p1);
+
+    }
+}
